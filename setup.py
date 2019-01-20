aa62d94d115f29b0011089b68a98d915d4967182
pau <paurosello@gmail.com>
2019-01-20 21:19:11 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
+import os, shutil
+from distutils.command.clean import clean as Clean
+
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in google_api_integration/__init__.py
+# get version from __version__ variable in frappe/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('google_api_integration/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
-requirements = parse_requirements("requirements.txt", session="")
+with open('google_api_integration/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='google_api_integration',
+    name='google_api_integration',
 	version=version,
 	description='Google API Integration',
 	author='Pau Rosello',
@@ -21,6 +24,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires,
+	dependency_links=[]
 )
